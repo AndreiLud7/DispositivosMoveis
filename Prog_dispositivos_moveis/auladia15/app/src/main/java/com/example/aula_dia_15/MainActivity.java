f1b59115157f02bb65c4bb6b80e4baedfcc8e456
andrei <andrei.m2005@aluno.ifsc.edu.br>
2024-04-05 09:52:19 -0300

    IMC completo

--- Prog_dispositivos_moveis/auladia15/app/src/main/java/com/example/aula_dia_15/MainActivity.java
+++ Prog_dispositivos_moveis/auladia15/app/src/main/java/com/example/aula_dia_15/MainActivity.java
@@ -26,24 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
         ebundle.putFloat("peso", Float.parseFloat(edpeso.getText().toString()));
         ebundle.putFloat("altura", Float.parseFloat(edaltura.getText().toString()));
-       // ebundle.putFloat("valorimc", Float.parseFloat(edpeso.getText().toString())/(Float.parseFloat(edaltura.getText().toString())*2));
         i.putExtras(ebundle);
 
         startActivity(i);
-
-
-        //double peso = Double.parseDouble(edpeso.getText().toString());
-      //  double altura = Double.parseDouble(edaltura.getText().toString());
-       // String IMC = String.valueOf(peso/(altura*2));
-
-       // Toast.makeText(this, "Resultado: "+IMC, Toast.LENGTH_SHORT).show();
-       // Log.d(IMC, "calcularIMC: ");
-
-      //  Intent intent = new Intent(this, Resultado.class);
-       // intent.putExtra("valorimc",IMC);
-       // startActivity(intent);
-
-
     }
+
 
 }
